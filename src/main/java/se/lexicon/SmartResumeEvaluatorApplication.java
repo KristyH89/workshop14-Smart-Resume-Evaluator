8e36a1badf612b842a40423235ac28d93d26df8a
Kristy Heijenk <ksheijenk@hotmail.com>
2026-08-25 11:09:08 +0200

    Add Spring Boot project structure with Spring AI dependencies

---
+++ src/main/java/se/lexicon/SmartResumeEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartResumeEvaluatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartResumeEvaluatorApplication.class, args);
+    }
+
+}
